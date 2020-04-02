f5e969db70a4a4e476ae210ee8820e4b75f1b4a3
ljs614 <sjl0614@naver.com>
2020-04-02 20:56:35 +0900

    Interface and Class in TS

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,23 @@
 "use strict";
+//interface는 ts에서만 적용.
+// interface Human {
+//     name: string,
+//     age: number,
+//     gender: string
+// }
 Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    ;
+}
+const lynn = new Human("Lynn", 18, "female");
+const sayHi = (person) => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 // arg? -> 선택적 변수. 없으면 undefined
-console.log(sayHi("jeongseop", 30, "male"));
+console.log(sayHi(person));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
-const sayHi = (name:string, age:number, gender:string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+//interface는 ts에서만 적용.
+// interface Human {
+//     name: string,
+//     age: number,
+//     gender: string
+// }
+
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender?: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    };
+}
+
+const lynn = new Human("Lynn", 18, "female");
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 // arg? -> 선택적 변수. 없으면 undefined
 
-console.log(sayHi("jeongseop", 30, "male"));
+console.log(sayHi(person));
 
 export {};
 
