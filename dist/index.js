6009e785461eac1614a7eaf018467131669a0576
ljs614 <sjl0614@naver.com>
2020-04-03 21:40:56 +0900

    Creating a Block

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,50 @@
 "use strict";
-//interface는 ts에서만 적용.
-// interface Human {
-//     name: string,
-//     age: number,
-//     gender: string
-// }
+// //interface는 ts에서만 적용.
+// // interface Human {
+// //     name: string,
+// //     age: number,
+// //     gender: string
+// // }
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+// class Human {
+//     public name: string;
+//     public age: number;
+//     public gender: string;
+//     constructor(name: string, age: number, gender?: string){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//     };
+// }
+// const lynn = new Human("Lynn", 18, "female");
+// const sayHi = (person: Human): string => {
+//     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+// };
+// // arg? -> 선택적 변수. 없으면 undefined
+// console.log(sayHi(person));
+const CryptoJS = require("crypto-js");
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
-    ;
 }
-const lynn = new Human("Lynn", 18, "female");
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+const genesisBlock = new Block(0, "2020202020", "", "Hello", 12345);
+let blockchain = [genesisBlock];
+const getBlockchain = () => blockchain;
+const getLatestBlock = () => blockchain[blockchain.length - 1];
+const getNewTimStamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimStamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
 };
-// arg? -> 선택적 변수. 없으면 undefined
-console.log(sayHi(person));
+console.log(createNewBlock("hello"), createNewBlock("byebye"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,83 @@
-//interface는 ts에서만 적용.
-// interface Human {
-//     name: string,
-//     age: number,
-//     gender: string
+// //interface는 ts에서만 적용.
+// // interface Human {
+// //     name: string,
+// //     age: number,
+// //     gender: string
+// // }
+
+// class Human {
+//     public name: string;
+//     public age: number;
+//     public gender: string;
+//     constructor(name: string, age: number, gender?: string){
+//         this.name = name;
+//         this.age = age;
+//         this.gender = gender;
+//     };
 // }
 
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age: number, gender?: string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-    };
+// const lynn = new Human("Lynn", 18, "female");
+
+// const sayHi = (person: Human): string => {
+//     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+// };
+// // arg? -> 선택적 변수. 없으면 undefined
+
+// console.log(sayHi(person));
+import * as CryptoJS from "crypto-js";
+
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+
+    static calculateBlockHash = (
+        index: number, 
+        previousHash: string, 
+        timestamp: number, 
+        data: string
+    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
 }
 
-const lynn = new Human("Lynn", 18, "female");
 
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-};
-// arg? -> 선택적 변수. 없으면 undefined
 
-console.log(sayHi(person));
+const genesisBlock:Block = new Block(0, "2020202020", "", "Hello", 12345);
 
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string) : Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimestamp: number = getNewTimStamp();
+    const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
+
+}
+
+console.log(createNewBlock("hello"), createNewBlock("byebye"));
 export {};
 
 
